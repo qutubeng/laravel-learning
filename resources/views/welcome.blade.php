849fd5ec0b5ba694a80984d5e01b03da702b6ece
Qutub Uddin Ahmed <qutubeng@gmail.com>
2017-02-14 18:31:10 +0100

    laravel learning project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Qutub
+                    Laravel Qutub Uddin
                 </div>
 
                 <div class="links">
